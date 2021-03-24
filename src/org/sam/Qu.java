078bc854fbc1fff27acb82404c0b9346f51330b4
Kanchana@21 <kanchsha1997@gmail.com>
2021-03-24 16:52:29 +0530

    A push

--- src/org/sam/Qu.java
+++ src/org/sam/Qu.java
@@ -3,5 +3,7 @@ package org.sam;
 public class Qu {
 public static void main(String[] args) {
 	System.out.println("java");
+	System.out.println("selenium");
+	System.out.println("frameworks");
 }
 }
